AOSP-bouquet/platform_device_xiaomi_bouquet

sdm660-common: Build QTI thermal HAL
Signed-off-by: Reinazhard <reinazhard@gmail.com>

sdm660.mk
@@ -381,6 +381,10 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     Settings \
     SystemUI
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.xiaomi_sdm660.qti
+
 # Tethering
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay

ancient-devices/device_asus_sdm660-common

sdm660.mk
@@ -470,6 +470,10 @@ PRODUCT_SYSTEM_VERITY_PARTITION=/dev/block/bootdevice/by-name/system
 PRODUCT_VENDOR_VERITY_PARTITION=/dev/block/bootdevice/by-name/vendor
 $(call inherit-product, build/target/product/verity.mk)
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.asus_sdm660.qti
+
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service