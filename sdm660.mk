306c4de099520cec7e62cf2b83b9d8291356ac8c
N4veenNK <naveen4264nk@gmail.com>
2022-02-10 06:47:23 +0530

    Revert "sdm660-common: Build aidl power stats and label it"
    This reverts commit abfbb3f795d1c2f719df307da13449bdbbdbd648.

--- sdm660.mk
+++ sdm660.mk
@@ -378,10 +378,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
-# Power Stats
-PRODUCT_PACKAGES += \
-    android.hardware.power.stats@1.0-service.mock
-
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
